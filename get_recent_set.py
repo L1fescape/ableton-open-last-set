42d4431388de44479ac22c7aa47649b496c417d3
Andrew Kennedy <akennedy2292@gmail.com>
2022-12-20 15:25:24 +0100

    add optional arg for specifying which live version to use

--- get_recent_set.py
+++ get_recent_set.py
@@ -1,15 +1,14 @@
-import re
-import os
-import logging
+import os, re, argparse, logging, getpass, string
 from sys import platform
-import getpass
-import string
 
-def parse_live_version(version):
-    major, minor, patch = re.search("Live (\d*)\.*(\d*)\.*(\d*)", version).groups()
+live_folder_prefix = 'Live '
+live_folder_regex = "{prefix}(\d*)\.*(\d*)\.*(\d*)".format(prefix=live_folder_prefix)
+
+def parse_live_version_from_folder_name(version):
+    major, minor, patch = re.search(live_folder_regex, version).groups()
     return int(major or 0), int(minor or 0), int(patch or 0)
 
-def get_ableton_preferences_filepath():
+def get_live_preferences_filepath(live_version=''):
     user = getpass.getuser()
     root_pref_folder = '/Users/{user}/Library/Preferences/Ableton/'.format(user=user)
     if platform == 'win32':
@@ -18,11 +17,16 @@ def get_ableton_preferences_filepath():
         logging.exception('Root directory "{folder}" does not exist'.format(folder=root_pref_folder))
         return
 
-    live_folders = [name for name in os.listdir(root_pref_folder) if os.path.isdir(os.path.join(root_pref_folder, name)) and 'Live ' in name]
+    live_folders = [name for name in os.listdir(root_pref_folder) if os.path.isdir(os.path.join(root_pref_folder, name)) and live_folder_prefix in name]
     if not live_folders:
-        logging.exception('There are no Ableton Live preferences folders in "{folder}" '.format(folder=root_pref_folder))
+        logging.exception('There are no Ableton Live preferences folders in "{folder}" that match format "{format}"'.format(folder=root_pref_folder, format=live_folder_regex))
         return
-    live_version = max(live_folders, key=parse_live_version) # pick the most recent version
+
+    # if a live version was specified via command args use that one, otherwise use the latest version
+    if live_version and live_folder_prefix not in live_version:
+        live_version = '{prefix}{version}'.format(prefix=live_folder_prefix, version=live_version)
+    else:
+        live_version = max(live_folders, key=parse_live_version_from_folder_name)
 
     pref_filename = '/Preferences.cfg'
     if platform == 'win32':
@@ -34,37 +38,50 @@ def get_ableton_preferences_filepath():
         pref_filename=pref_filename
     )
 
-prefs_filepath = get_ableton_preferences_filepath()
-if not prefs_filepath:
-    exit()
-if not os.path.exists(prefs_filepath):
-    logging.exception('Attempted to use preferences file "{filepath}" but it does not exist'.format(filepath=prefs_filepath))
-    exit()
+def get_recent_set(live_version=''):
+    recent_set = None
 
-recent_set = None
+    prefs_filepath = get_live_preferences_filepath(live_version)
+    if not prefs_filepath:
+        return
+    if not os.path.exists(prefs_filepath):
+        logging.exception('Attempted to use preferences file "{filepath}" but the file does not exist'.format(filepath=prefs_filepath))
+        return
 
-# Preferences.cfg is a binary file and this is the horrible way it's being parsed at the moment.
-# It looks like this file has some sort of xml structure. It would be great to be able to parse
-# that and reference keys to get the most recent file instead of the method below
-with open(prefs_filepath, mode='rb') as file:
-    prefs_content = file.read().decode('latin1')
+    # Preferences.cfg is a binary file and this is the horrible way it's being parsed at the moment.
+    # It looks like this file has some sort of xml structure. It would be great to be able to parse
+    # that and reference keys to get the most recent file instead of the method below
+    with open(prefs_filepath, mode='rb') as file:
+        prefs_content = file.read().decode('latin1')
 
-# attempt to filter out a lot of the garbage we get back after reading the file in as binary.
-# filtering on `string.printable` is a temporary solution and will cause problems for set names
-# that have unicode or emoji in them
-filtered_prefs_content = ''
-for c in prefs_content:
-    if c in set(string.printable):
-        filtered_prefs_content += c
+    # attempt to filter out a lot of the garbage we get back after reading the file in as binary.
+    # filtering on `string.printable` is a temporary solution and will cause problems for set names
+    # that have unicode or emoji in them
+    filtered_prefs_content = ''
+    for c in prefs_content:
+        if c in set(string.printable):
+            filtered_prefs_content += c
 
-# split on 'FileRef' and then look for the first item containing '.als'
-for chunk in filtered_prefs_content.split('FileRef'):
-    if chunk.find('.als') > -1:
-        recent_set = chunk[chunk.find('/'):] # remove random chars infront of each path
-        break
+    # split on 'FileRef' and then look for the first item containing '.als'
+    for chunk in filtered_prefs_content.split('FileRef'):
+        if chunk.find('.als') > -1:
+            recent_set = chunk[chunk.find('/'):] # remove random chars infront of each path
+            break
 
-if not recent_set:
-    logging.exception('Preferences file could not be found')
-    exit()
+    if not recent_set:
+        logging.exception('Recent set could not be found in preferences file "{filepath}"'.format(filepath=prefs_filepath))
+        return
 
-print(recent_set)
+    return recent_set
+
+def main():
+    parser = argparse.ArgumentParser(description='Display the filepath of the most recent Ableton Live set by parsing Ableton\'s Preferences.cfg.')
+    parser.add_argument("-v", "--live_version", help = "Preferred Live version to use (defaults to the latest version if omitted)", required = False, default = "")
+    argument = parser.parse_args()
+
+    recent_set = get_recent_set(argument.live_version)
+    if recent_set:
+        print(recent_set)
+
+if __name__ == "__main__":
+   main()
--- osx/Ableton Recent.app
+++ osx/Ableton Recent.app
@@ -1,20 +1,16 @@
--- source: https://stackoverflow.com/a/3469708
-on FileExists(theFile) -- (String) as Boolean
+-- credit: https://stackoverflow.com/a/3469708
+on FileExists(theFile)
 	tell application "System Events"
-		if exists file theFile then
-			return true
-		else
-			return false
-		end if
+		return exists file theFile
 	end tell
 end FileExists
 
 on run
-	set getRecentSetResult to do shell script "/usr/local/bin/python /path/to/get_recent_set.py 2>&1"
-	if FileExists(getRecentSetResult) then
-		do shell script "open \"" & getRecentSetResult & "\""
+	set recentSet to do shell script "/usr/local/bin/python /path/to/get_recent_set.py 2>&1"
+	if FileExists(recentSet) then
+		do shell script "open \"" & recentSet & "\""
 		tell application "Ableton Live 11 Suite" to activate
 	else
-		display dialog "Could not get a recent file:\n" & getRecentSetResult
+		display dialog "Could not get a recent file:\n" & recentSet
 	end if
 end run
